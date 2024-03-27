e4c4523e8ed4b21326533abdc9229702ff8aec8d
kotomoto <jkotor@gmail.com>
2024-03-27 23:18:57 +0100

    Add meal details screen and navigation to it

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -5,9 +5,14 @@ import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
-  const MealItem({super.key, required this.meal});
+  const MealItem({
+    super.key,
+    required this.meal,
+    required this.onSelectMeal,
+  });
 
   final Meal meal;
+  final void Function(Meal meal) onSelectMeal;
 
   String get complexityText {
     return toBeginningOfSentenceCase(meal.complexity.name);
@@ -25,7 +30,9 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          onSelectMeal(meal);
+        },
         child: Stack(
           children: [
             FadeInImage(
