be720e27b2a6ba3e4f1cada083d31972aabcf898
kotomoto <jkotor@gmail.com>
2024-04-09 15:59:12 +0200

    Handle icons for favorite meals

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -13,6 +13,9 @@ class MealDetailsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    var favoriteMeals = ref.watch(favoriteMealsProvider);
+    final isFavorite = favoriteMeals.contains(meal);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
@@ -27,7 +30,7 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: const Icon(Icons.star),
+            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
           )
         ],
       ),
